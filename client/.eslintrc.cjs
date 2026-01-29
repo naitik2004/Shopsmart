aba46ba416228e2c151ff2778126def67034c5c1
Noick <naitik1234567890pandey@gmail.com>
2026-01-29 12:30:15 +0530

    Add CI workflow and client lint configuration

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["react"],
+  settings: {
+    react: {
+      version: "detect"
+    }
+  },
+  rules: {}
+};
+
